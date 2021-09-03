97a23290cf313e088e9ab99615e6bacc1dd2b06f
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-09-03 09:52:57 +0300

    fixing category iteration

--- shop/views.py
+++ shop/views.py
@@ -9,7 +9,7 @@ def product_list(request, category_slug=None):
     products = Product.objects.filter(available=True)
 
     if category_slug:
-        category = get_object_or_404(Category, category_slug, )
+        category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
     contxt = {
