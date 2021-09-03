b0ea01ea854cb7a4a95366ed39a197c841dc4019
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-09-03 10:52:23 +0300

    added product detail

--- shop/views.py
+++ shop/views.py
@@ -24,5 +24,5 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id,
                                 slug=slug,
                                 available=True)
-    return render(request, 'shop/product/detail',
+    return render(request, 'shop/product/detail.html',
                   {'product': product})
