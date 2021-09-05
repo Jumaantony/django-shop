f23386b40816efad0a1ca414725a257123fb1aed
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-09-05 09:37:28 +0300

    created app to register customer orders

--- myshop/urls.py
+++ myshop/urls.py
@@ -26,6 +26,10 @@ urlpatterns = [
 
     # shop url
     path('', include('shop.urls', namespace='shop')),
+
+    # order url
+    path('orders/', include('orders.urls', namespace='orders')),
+
 ]
 if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL,
                                          document_root=settings.MEDIA_ROOT)
