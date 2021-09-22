66f2fac653e43ef92b59d2e779886957b41e0cad
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-09-22 19:09:46 +0300

    generating pdf invoices dynamically

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,5 @@
+import os
+from django.contrib.staticfiles import finders
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
@@ -5,6 +7,10 @@ from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
 from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string, get_template
+from xhtml2pdf import pisa
 
 
 # Create your views here.
@@ -39,9 +45,63 @@ def order_create(request):
                    'form': form})
 
 
+# adding order details to the admin
 @staff_member_required()
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
+
     return render(request,
                   'admin/orders/order/detail.html',
                   {'order': order})
+
+
+# generating pdf invoice
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    template_path = 'orders/order/pdf.html'
+    context = {'order': order}
+
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename=order_{order.id}.pdf'
+
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+        html, dest=response, link_callback=link_callback)
+    # if error then show some funy view
+    if pisa_status.err:
+        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+
+def link_callback(uri, rel):
+    global sUrl, mUrl
+    result = finders.find(uri)
+    if result:
+        if not isinstance(result, (list, tuple)):
+            result = [result]
+        result = list(os.path.realpath(path) for path in result)
+        path = result[0]
+    else:
+        sUrl = settings.STATIC_URL  # Typically /static/
+        sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL  # Typically /media/
+        mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        else:
+            return uri
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+        raise Exception(
+            f'media URI must start with {sUrl} or {mUrl}')
+    return path
